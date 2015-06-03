8b8e613b697a1de2a2fcc0a6d496fd8414e21bc5
Josh Rolstad <jrolstad@gmail.com>
2015-06-03 06:57:58 -0700

    Adding use case examples

---
+++ fake_rabbit.tests/UseCases/ReceiveMessages.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using fake_rabbit.models;
+using NUnit.Framework;
+using RabbitMQ.Client;
+
+namespace fake_rabbit.tests.UseCases
+{
+    [TestFixture]
+    public class ReceiveMessages
+    {
+        [Test]
+        public void ReceiveMessagesOnQueue()
+        {
+            var rabbitServer = new RabbitServer();
+
+            ConfigureQueueBinding(rabbitServer, "my_exchange", "my_queue");
+            SendMessage(rabbitServer,"my_exchange","hello_world");
+
+            var connectionFactory = new FakeConnectionFactory(rabbitServer);
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // First message
+                var message = channel.BasicGet("my_queue", noAck: false);
+                
+                Assert.That(message,Is.Not.Null);
+                var messageBody = Encoding.ASCII.GetString(message.Body);
+
+                Assert.That(messageBody,Is.EqualTo("hello_world"));
+
+                channel.BasicAck(message.DeliveryTag,multiple:false);
+            }
+
+        }
+
+        private static void SendMessage(RabbitServer rabbitServer, string exchange, string message)
+        {
+            var connectionFactory = new FakeConnectionFactory(rabbitServer);
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var messageBody = Encoding.ASCII.GetBytes(message);
+                channel.BasicPublish(exchange: exchange, routingKey: null, mandatory: false, basicProperties: null,body: messageBody);
+            }
+        }
+
+        private void ConfigureQueueBinding(RabbitServer rabbitServer, string exchangeName, string queueName)
+        {
+            var connectionFactory = new FakeConnectionFactory(rabbitServer);
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Direct);
+
+                channel.QueueBind(queueName, exchangeName, null);
+            }
+        }
+    }
+}
---
+++ fake_rabbit.tests/UseCases/SendMessages.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using System.Threading;
+using fake_rabbit.models;
+using NUnit.Framework;
+using RabbitMQ.Client;
+
+namespace fake_rabbit.tests.UseCases
+{
+    [TestFixture]
+    public class SendMessages
+    {
+        [Test]
+        public void SendToExchangeOnly()
+        {
+            var rabbitServer = new RabbitServer();
+            var connectionFactory = new FakeConnectionFactory(rabbitServer);
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                const string message = "hello world!";
+                var messageBody = Encoding.ASCII.GetBytes(message);
+                channel.BasicPublish(exchange:"my_exchange",routingKey:null,mandatory:false,basicProperties:null,body:messageBody);
+            }
+
+            Assert.That(rabbitServer.Exchanges["my_exchange"].Messages.Count,Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SendToExchangeWithBoundQueue()
+        {
+            var rabbitServer = new RabbitServer();
+            var connectionFactory = new FakeConnectionFactory(rabbitServer);
+
+            ConfigureQueueBinding(rabbitServer,"my_exchange","some_queue");
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                const string message = "hello world!";
+                var messageBody = Encoding.ASCII.GetBytes(message);
+                channel.BasicPublish(exchange: "my_exchange", routingKey: null, mandatory: false, basicProperties: null, body: messageBody);
+            }
+
+            Assert.That(rabbitServer.Queues["some_queue"].Messages.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SendToExchangeWithMultipleBoundQueues()
+        {
+            var rabbitServer = new RabbitServer();
+            var connectionFactory = new FakeConnectionFactory(rabbitServer);
+
+            ConfigureQueueBinding(rabbitServer, "my_exchange", "some_queue");
+            ConfigureQueueBinding(rabbitServer, "my_exchange", "some_other_queue");
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                const string message = "hello world!";
+                var messageBody = Encoding.ASCII.GetBytes(message);
+                channel.BasicPublish(exchange: "my_exchange", routingKey: null, mandatory: false, basicProperties: null, body: messageBody);
+            }
+
+            Assert.That(rabbitServer.Queues["some_queue"].Messages.Count, Is.EqualTo(1));
+            Assert.That(rabbitServer.Queues["some_other_queue"].Messages.Count, Is.EqualTo(1));
+        }
+
+        private void ConfigureQueueBinding(RabbitServer rabbitServer, string exchangeName, string queueName)
+        {
+            var connectionFactory = new FakeConnectionFactory(rabbitServer);
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue:queueName,durable:false,exclusive:false,autoDelete:false,arguments:null);
+                channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Direct);
+
+                channel.QueueBind(queueName,exchangeName,null);
+            }
+        }
+    }
+}
